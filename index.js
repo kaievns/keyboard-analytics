15f69caf0d30c29a1603c6773325a04a45a96939
Nikolay Nemshilov <nemshilov@gmail.com>
2016-09-26 20:51:24 +1000

    Setting up a simple video capture setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const video = document.getElementById('video');
 
+navigator.getUserMedia(
+  { video: true },
+  stream => video.src = URL.createObjectURL(stream),
+  error => console.log('failed', error)
+);
