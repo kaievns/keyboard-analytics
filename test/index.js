9c0807a2fcbb4c8e559cef28528136246bc92cad
Nikolay Nemshilov <nemshilov@gmail.com>
2016-10-06 21:12:32 +1100

    Adding the in key-caps measure to everything

--- test/index.js
+++ test/index.js
@@ -30,6 +30,7 @@ const handPositionWhenPressed = (letters=[], x='x1', y='y1') => {
   };
 };
 
+
 const LEFT_HAND_SYMBOLS = '`12345qwertasdfgzxcvb'.split('');
 const RIGHT_HAND_SYMBOLS = '67890-=yuiop[]\\hjkl;\'nm,./'.split('').concat(['Enter']);
 const SHIFTED_LEFT_LETTERS = 'QWERTASDFGZXCVB'.split('').filter(key => data[key]);
@@ -39,14 +40,20 @@ const SHIFTED_LETTERS = SHIFTED_LEFT_LETTERS.concat(SHIFTED_RIGHT_LETTERS);
 const LEFT_HAND_POS = handPositionWhenPressed(RIGHT_HAND_SYMBOLS, 'x1', 'y1');
 const RIGHT_HAND_POS = handPositionWhenPressed(LEFT_HAND_SYMBOLS, 'x2', 'y2');
 
+const HAND_DIST_IN_KEY_CAPS = 8;
+const KEY_CAPS_DENOMINATOR = (RIGHT_HAND_POS.x - LEFT_HAND_POS.x) / HAND_DIST_IN_KEY_CAPS;
+
+const toCaps = dist => parseFloat((dist / KEY_CAPS_DENOMINATOR).toFixed(2));
+
 const NORMAL_LETTERS_DATA = Object.keys(data).reduce((list, key) => {
   if (!SHIFTED_LETTERS.includes(key)) {
     const anchor = LEFT_HAND_SYMBOLS.includes(key) ? LEFT_HAND_POS : RIGHT_HAND_POS;
     const position = avgPosition(key);
     const diff = diffPosition(position, anchor);
     const dist = distance(diff);
+    const caps = toCaps(dist);
 
-    list.push({ key, position, diff, dist });
+    list.push({ key, position, diff, dist, caps });
   }
 
   return list;
@@ -64,10 +71,17 @@ NORMAL_LETTERS_DATA.forEach(entry => {
     `"${entry.key}"`,
     '  |  pos:', entry.position,
     '  |  diff:', entry.diff,
-    '  |  dist:', entry.dist
+    '  |  dist:', entry.dist,
+    '  |  caps:', entry.caps
   );
 });
 
 console.log('');
-console.log('Left shift pos:', LEFT_SHIFT_POS, diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS), distance(diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS)));
-console.log('Right shift pos: ', RIGHT_SHIFT_POS, diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS), distance(diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS)));
+console.log('Left shift pos:', LEFT_SHIFT_POS,
+  diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS),
+  distance(diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS)),
+  toCaps(distance(diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS))));
+console.log('Right shift pos: ', RIGHT_SHIFT_POS,
+  diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS),
+  distance(diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS)),
+  toCaps(distance(diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS))));
