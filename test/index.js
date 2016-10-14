4f342e3d0da3e938cb5ee18f983436936f9da77b
Nikolay Nemshilov <nemshilov@gmail.com>
2016-10-14 16:24:09 +1100

    Reworking the test script to make it read a collection of data sets

--- test/index.js
+++ test/index.js
@@ -1,99 +1,125 @@
-const data = require('../data');
+const fs = require('fs');
 
-const avg = entries =>
-  ~~ (entries.reduce((sum, item) => sum + item, 0) / entries.length);
+const DATA_DIR = `${__dirname}/data`;
+const HAND_DIST_IN_KEY_CAPS = 8;
+const LEFT_HAND_SYMBOLS = '`12345qwertasdfgzxcvb'.split('');
+const RIGHT_HAND_SYMBOLS = '67890-=yuiop[]\\hjkl;\'nm,./'.split('').concat(['Enter']);
+const SHIFTED_LEFT_LETTERS = 'QWERTASDFGZXCVB'.split('');
+const SHIFTED_RIGHT_LETTERS = 'YUIOPHJKL:NM<>?'.split('');
+const SHIFTED_LETTERS = SHIFTED_LEFT_LETTERS.concat(SHIFTED_RIGHT_LETTERS);
 
-const distance = vector => Math.round(Math.sqrt(vector.x * vector.x + vector.y * vector.y));
+let data;
+let key_caps_denominator;
 
-const avgPosition = key => {
+console.log('Reading data sets:');
+const DATA_FILES = fs.readdirSync(DATA_DIR);
+const DATA_SETS = DATA_FILES.reduce((sets, filename) => {
+  console.log(` * ${filename}`);
+  return Object.assign(sets, { [filename]: require(`${DATA_DIR}/${filename}`) });
+}, {});
+
+console.log("\nAnalyzing the data:");
+
+const results = Object.keys(DATA_SETS).map(filename => {
+  console.log(` * ${filename}`);
+  return analyzeDataSet(DATA_SETS[filename]);
+});
+
+console.log("\nHere's what i've got now:");
+results.forEach(prettyPrint)
+
+function prettyPrint(result) {
+  let keyboard = "\n"+
+  " \` 1 2 3 4 5 6 7 8 9 0 - = \n"+
+  "    q w e r t y u i o p [ ] \\ \n"+
+  "    a s d f g h j k l ; ' Enter \n"+
+  "     z x c v b n m , . / \n"+
+  " l-shift              00                 r-shift "
+  ;
+
+  result.forEach(entry => {
+    keyboard = keyboard.replace(` ${entry.key} `, ` ${zerofy(entry.dist)} `);
+  });
+
+  console.log(keyboard);
+}
+
+function analyzeDataSet(dataset) {
+  data = dataset;
+
+  const left_hand_pos = handPositionWhenPressed(RIGHT_HAND_SYMBOLS, 'x1', 'y1');
+  const right_hand_pos = handPositionWhenPressed(LEFT_HAND_SYMBOLS, 'x2', 'y2');
+
+  key_caps_denominator = (right_hand_pos.x - left_hand_pos.x) / HAND_DIST_IN_KEY_CAPS;
+
+  const normal_letter_data = Object.keys(data).reduce((list, key) => {
+    if (!SHIFTED_LETTERS.includes(key)) {
+      const anchor = LEFT_HAND_SYMBOLS.includes(key) ? left_hand_pos : right_hand_pos;
+      const position = avgPosition(key);
+      const diff = diffPosition(position, anchor);
+      const dist = distance(diff);
+
+      list.push({ key, position, diff, dist });
+    }
+
+    return list;
+  }, []);
+
+  const left_shift_pos = handPositionWhenPressed(SHIFTED_RIGHT_LETTERS, 'x1', 'y1');
+  const right_shift_pos = handPositionWhenPressed(SHIFTED_LEFT_LETTERS, 'x2', 'y2');
+  const left_shift_diff = diffPosition(left_shift_pos, left_hand_pos);
+  const right_shift_diff = diffPosition(right_shift_pos, right_hand_pos);
+
+  const all_data = normal_letter_data.concat([
+    { key: 'l-shift', position: left_shift_pos, diff: left_shift_diff, dist: distance(left_shift_diff) },
+    { key: 'r-shift', position: right_shift_pos, diff: right_shift_diff, dist: distance(right_shift_diff) }
+  ]);
+
+  return all_data;
+}
+
+function zerofy(val) {
+  return val > 9 ? val : `0${val}`;
+}
+
+function avg(entries) {
+  return ~~ (entries.reduce((sum, item) => sum + item, 0) / entries.length);
+}
+
+function distance(vector) {
+  return Math.round(Math.sqrt(vector.x * vector.x + vector.y * vector.y));
+}
+
+function avgPosition(key) {
   const [x, y] = LEFT_HAND_SYMBOLS.includes(key) ? ['x1', 'y1'] : ['x2', 'y2'];
+  return { x: avg(data[key][x]), y: avg(data[key][y]) };
+}
+
+function toCapSizes(dist) {
+  return parseFloat((dist / key_caps_denominator * 10).toFixed(1));
+}
+
+function diffPosition(pos1, pos2) {
   return {
-    x: avg(data[key][x]),
-    y: avg(data[key][y])
+    x: toCapSizes(Math.abs(pos1.x - pos2.x)),
+    y: toCapSizes(Math.abs(pos1.y - pos2.y))
   };
 }
 
-const diffPosition = (pos1, pos2) => ({
-  x: toCapSizes(Math.abs(pos1.x - pos2.x)),
-  y: toCapSizes(Math.abs(pos1.y - pos2.y))
-});
+function handPositionWhenPressed(letters=[], x='x1', y='y1') {
+  const positions = letters.reduce((sum, key) => {
+    if (data[key]) {
+      return {
+        x: sum.x.concat(data[key][x]),
+        y: sum.y.concat(data[key][y])
+      };
+    }
 
-const handPositionWhenPressed = (letters=[], x='x1', y='y1') => {
-  const positions = letters.reduce((sum, key) => ({
-    x: sum.x.concat(data[key][x]),
-    y: sum.y.concat(data[key][y])
-  }), { x: [], y: [] });
+    return sum;
+  }, { x: [], y: [] });
 
   return {
     x: avg(positions.x),
     y: avg(positions.y)
   };
-};
-
-
-const LEFT_HAND_SYMBOLS = '`12345qwertasdfgzxcvb'.split('');
-const RIGHT_HAND_SYMBOLS = '67890-=yuiop[]\\hjkl;\'nm,./'.split('').concat(['Enter']);
-const SHIFTED_LEFT_LETTERS = 'QWERTASDFGZXCVB'.split('').filter(key => data[key]);
-const SHIFTED_RIGHT_LETTERS = 'YUIOPHJKL:NM<>?'.split('').filter(key => data[key]);
-const SHIFTED_LETTERS = SHIFTED_LEFT_LETTERS.concat(SHIFTED_RIGHT_LETTERS);
-
-const LEFT_HAND_POS = handPositionWhenPressed(RIGHT_HAND_SYMBOLS, 'x1', 'y1');
-const RIGHT_HAND_POS = handPositionWhenPressed(LEFT_HAND_SYMBOLS, 'x2', 'y2');
-
-const HAND_DIST_IN_KEY_CAPS = 8;
-const KEY_CAPS_DENOMINATOR = (RIGHT_HAND_POS.x - LEFT_HAND_POS.x) / HAND_DIST_IN_KEY_CAPS;
-
-const toCapSizes = dist => parseFloat((dist / KEY_CAPS_DENOMINATOR * 10).toFixed(1));
-
-const NORMAL_LETTERS_DATA = Object.keys(data).reduce((list, key) => {
-  if (!SHIFTED_LETTERS.includes(key)) {
-    const anchor = LEFT_HAND_SYMBOLS.includes(key) ? LEFT_HAND_POS : RIGHT_HAND_POS;
-    const position = avgPosition(key);
-    const diff = diffPosition(position, anchor);
-    const dist = distance(diff);
-
-    list.push({ key, position, diff, dist });
-  }
-
-  return list;
-}, []);
-
-const LEFT_SHIFT_POS = handPositionWhenPressed(SHIFTED_RIGHT_LETTERS, 'x1', 'y1');
-const RIGHT_SHIFT_POS = handPositionWhenPressed(SHIFTED_LEFT_LETTERS, 'x2', 'y2');
-
-console.log('Left hand pos:', LEFT_HAND_POS);
-console.log('Right hand pos:', RIGHT_HAND_POS);
-console.log('');
-
-NORMAL_LETTERS_DATA.forEach(entry => {
-  console.log(
-    `"${entry.key}"`,
-    '  |  pos:', entry.position,
-    '  |  diff:', entry.diff,
-    '  |  dist:', entry.dist
-  );
-});
-
-console.log('');
-console.log('Left shift pos:', LEFT_SHIFT_POS,
-  diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS),
-  distance(diffPosition(LEFT_SHIFT_POS, LEFT_HAND_POS)));
-console.log('Right shift pos: ', RIGHT_SHIFT_POS,
-  diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS),
-  distance(diffPosition(RIGHT_SHIFT_POS, RIGHT_HAND_POS)));
-
-
-let keyboard = "\n"+
-" \` 1 2 3 4 5 6 7 8 9 0 - = \n"+
-"    q w e r t y u i o p [ ] \\ \n"+
-"    a s d f g h j k l ; ' Enter \n"+
-"     z x c v b n m , . / "
-;
-
-const zerofy = val => val > 9 ? val : `0${val}`;
-
-NORMAL_LETTERS_DATA.forEach(entry => {
-  keyboard = keyboard.replace(` ${entry.key} `, ` ${zerofy(entry.dist)} `);
-});
-
-console.log(keyboard);
+}
